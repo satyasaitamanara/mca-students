389d34fa72edae0cf53906b2ce69cbefaf7959c6
satyasaitamanara <tamanarasatyasai@gmail.com>
2025-03-10 22:53:03 +0530

    Update index.js

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(cors());
 app.use(bodyParser.json());
 
 // Database Connection
-const db = mysql.createpool({
+const db = mysql.createPool({
     host:"bybimdiekjxs5r5y3bm6-mysql.services.clever-cloud.com",
     user:"ulpcyfzuikf5vyok",
     password:"Ov494YIrJ7EmCXGR27Gf",
